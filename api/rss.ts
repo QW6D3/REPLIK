214f61f6c4653a8f7041f29e6a1d735ee37c1d67
Niesmer <antoine.ferron.pro@pm.me>
2024-10-22 16:04:03 +0200

    WIP : Insertion fichier dans bdd
    Connexion à la base de données
    Commencement flux RSS
    Stockage fichier locale dans BDD

---
+++ api/rss.ts
@@ -0,0 +1,45 @@
+import {
+  MongoClient,
+  GridFSBucket,
+  type Db,
+  type MongoClient as MongoClientType,
+} from 'mongodb'
+import fs, { PathLike } from 'fs'
+
+async function connectMongo(): Promise<
+  MongoClientType | { error: string; details: string }
+> {
+  const client = new MongoClient('mongodb://localhost:27017')
+  try {
+    await client.connect()
+    return client
+  } catch (error) {
+    console.error('Connection failed', error)
+    return { error: 'Connection failed', details: (error as Error).message }
+  }
+}
+
+export async function updateRss(): Promise<void> {
+  const client = await connectMongo()
+  if ('error' in client) {
+    return
+  }
+}
+
+async function uploadFile(filePath: PathLike, fileName: string): Promise<void> {
+  const client = await connectMongo()
+  if ('error' in client) {
+    return
+  }
+
+  const db: Db = client.db('replik')
+  const bucket = new GridFSBucket(db)
+
+  const uploadStream = bucket.openUploadStream(fileName)
+  const fileStream = fs.createReadStream(filePath)
+  fileStream.pipe(uploadStream).on('finish', () => {
+    console.log('File uploaded')
+  })
+}
+
+uploadFile('../test.txt', 'test.txt')
