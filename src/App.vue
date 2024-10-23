406a4d64f7bbb0df92fa19f434a316d494acb133
GGab1 <149067546+GGab1@users.noreply.github.com>
2024-10-23 09:20:47 +0200

    Ajout page liste de podcasts et modification header

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Accueil</RouterLink>
+        <RouterLink to="/podcasts">Liste des podcasts</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/PodcastsView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="podcasts">
+    <h1>Test</h1>
+  </div>
+</template>
