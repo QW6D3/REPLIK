225a1b0b917e3fe910d425fc8128bf17fd4b134f
GGab1 <149067546+GGab1@users.noreply.github.com>
2024-11-05 10:17:34 +0100

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import Footer from './components/AppFooter.vue'
 </script>
 
 <template>
---
+++ src/views/PodcastsView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="podcasts">
+    <h1>Test</h1>
+  </main>
+</template>
