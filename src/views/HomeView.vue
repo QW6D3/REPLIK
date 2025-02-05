0560e2cda07594659c8c836dd02d96cc0ae42030
Niesmer <antoine.ferron.pro@pm.me>
2025-02-05 16:11:55 +0100

    Switched classic js function to Vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,46 @@
 <template>
   <main class="pageAccueil">
-    <div class="lastRelease">
+    <div class="lastRelease" v-if="latestPodcast">
       <div class="descPodcast">
         <h2>Dernière sortie</h2>
-        <p id="descContent"></p>
-        <a id="playLink">
+        <p>{{ latestPodcast.description || "Description indisponible" }}</p>
+        <a :href="`/podcasts/${latestPodcast.id}`">
           <img src="../assets/playButton.png" alt="Play Button" class="playIcon">
         </a>
       </div>
-      <img src="" alt="Profil Podcast" class="profilImage">
+      <img :src="latestPodcast.image" alt="Profil Podcast" class="profilImage">
     </div>
 
-    <div id="podcastGrid" class="listPodcast">
+    <div class="listPodcast">
+      <a v-for="podcast in podcasts" :key="podcast.id" :href="`/podcasts/${podcast.id}`">
+        <img :src="podcast.image" :alt="`Illustration de ${podcast.title}`" :width="200" :height="200"
+          style="object-fit: cover; border-radius: 10px;">
+      </a>
     </div>
   </main>
 </template>
+
+<script>
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      podcasts: []
+    }
+  },
+  computed: {
+    latestPodcast() {
+      return this.podcasts[0] || null
+    }
+  },
+  async created() {
+    try {
+      const response = await fetch('podcasts.json')
+      const data = await response.json()
+      this.podcasts = data.podcasts
+    } catch (error) {
+      console.error("Erreur de chargement du fichier JSON :", error)
+    }
+  }
+}
+</script>
